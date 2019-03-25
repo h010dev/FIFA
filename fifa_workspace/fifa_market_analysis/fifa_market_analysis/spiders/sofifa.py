f586eb478271d3b4bbb9a317b78f3a2de3a4de5c
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-03-25 10:28:39 +0700

    Changed settings.py to bypass 403 error.

--- fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/sofifa.py
+++ fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/sofifa.py
@@ -11,13 +11,15 @@ class SofifaSpider(scrapy.Spider):
             'https://sofifa.com/players'
         ]
 
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
+
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, headers=headers, callback=self.parse)
 
     def parse(self, response):
 
         page = response.url
-        _file = f'{page}.html'
+        _file = 'sofifa.html'
         with open(_file, 'wb') as f:
             f.write(response.body)
 
