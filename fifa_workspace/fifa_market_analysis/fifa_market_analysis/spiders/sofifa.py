9dcaf825af30dec1d119b49e65d6e8aaa9cc2908
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-03-25 10:41:44 +0700

    Changes made to parse function: now retrieves a list of stats from all pages.

--- fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/sofifa.py
+++ fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/sofifa.py
@@ -18,9 +18,31 @@ class SofifaSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        page = response.url
-        _file = 'sofifa.html'
-        with open(_file, 'wb') as f:
-            f.write(response.body)
+        for row in response.selector.xpath("//table[@class='table table-hover persist-area']/tbody/tr"):
+            yield {
+                # 'id': row.xpath(".//tbody//figure[@class='avatar']//img[contains(@id, '')]").extract_first(),
+                'name': row.xpath(".//a[contains(@href, '/player/')]/text()[1]").extract_first(),
+                'position': row.xpath(".//span[contains(@class, 'pos')]/text()[1]").extract(),
+                'age': row.xpath(".//*[@class='col-digit col-ae']/text()[1]").extract_first(),
+                'overall': row.xpath(".//*[@class='col-digit col-oa']/span/text()[1]").extract_first(),
+                'potential': row.xpath(".//*[@class='col-digit col-pt']/span/text()[1]").extract_first(),
+                'team': row.xpath(".//a[contains(@href, '/team/')]/text()[1]").extract_first(),
+                'contract': row.xpath(".//div[@class='subtitle text-ellipsis rtl']/text()[1]").extract_first(),
+                'value': row.xpath(".//div[@class='col-digit col-vl']/text()[1]").extract_first(),
+                'wage': row.xpath(".//div[@class='col-digit col-wg']/text()[1]").extract_first(),
+                'total_stats': row.xpath(".//div[@class='col-digit col-tt']/text()[1]").extract_first(),
+                'hits_comments': row.xpath(".//div[@class='col-comments text-right text-ellipsis rtl']"
+                                           "/text()[1]").extract_first()
+            }
 
+        second_page = response.selector.xpath(f"//a[@class='btn pjax'][{1}]/@href").extract_first()
+        next_page = response.selector.xpath(f"//a[@class='btn pjax'][{2}]/@href").extract_first()
 
+        if next_page is None:
+            # Used for first page only, since there is no previous button
+            second_page_link = response.urljoin(second_page)
+            yield scrapy.Request(url=second_page_link, callback=self.parse)
+
+        elif next_page is not None:
+            next_page_link = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_link, callback=self.parse)
