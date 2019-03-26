9b2ff1a3f461c486cdc972e6fcf58dad188c621d
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-03-26 11:11:40 +0700

    Modified pipelines.py to accept preset uri and database path.

--- fifa_workspace/fifa_market_analysis/fifa_market_analysis/pipelines.py
+++ fifa_workspace/fifa_market_analysis/fifa_market_analysis/pipelines.py
@@ -11,9 +11,16 @@ class MongoDBPipeline(object):
 
     collection = 'fifa'
 
-    def __init__(self):
-        self.mongo_uri = 'mongodb://localhost:27017'
-        self.mongo_db = 'sofifa'
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DB')
+        )
 
     def open_spider(self, spider):
         self.client = MongoClient(self.mongo_uri)
