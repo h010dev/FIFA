8fcbeaa4551c3fc4ddeb6928d641ba186f902b01
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-03-26 11:02:10 +0700

    Modified pipelines.py: sofifa will now dump data to mongodb.

--- fifa_workspace/fifa_market_analysis/fifa_market_analysis/pipelines.py
+++ fifa_workspace/fifa_market_analysis/fifa_market_analysis/pipelines.py
@@ -4,8 +4,24 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from pymongo import MongoClient
 
 
-class FifaMarketAnalysisPipeline(object):
+class MongoDBPipeline(object):
+
+    collection = 'fifa'
+
+    def __init__(self):
+        self.mongo_uri = 'mongodb://localhost:27017'
+        self.mongo_db = 'sofifa'
+
+    def open_spider(self, spider):
+        self.client = MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.db[self.collection].insert_one(dict(item))
         return item
