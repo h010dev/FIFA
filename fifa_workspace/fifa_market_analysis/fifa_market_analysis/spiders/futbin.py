eadff2d738b6a11c1312db2618c919807140fd90
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-03-27 12:23:46 +0700

    Added parse functionality with lua scripts for futbin spider. Returns empty list at this point.

--- fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/futbin.py
+++ fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/futbin.py
@@ -1,11 +1,46 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy_splash import SplashRequest
+from scrapy.selector import Selector
 
 
 class FutbinSpider(scrapy.Spider):
     name = 'futbin'
-    allowed_domains = ['www.futbin.com/market/auctions']
-    start_urls = ['http://www.futbin.com/market/auctions/']
+
+    script = """
+    function main(splash, args)
+      assert(splash:go(args.url))
+      assert(splash:wait(5))
+      assert(splash:runjs('document.querySelector(".btn[href$=XONE]").href'))
+      splash:set_viewport_full()
+      return {
+        html = splash:html(),
+        png = splash:png(),
+        har = splash:har(),
+      }
+    end
+    """
+
+    def start_request(self):
+        url = 'https://www.futbin.com/market/auctions'
+        yield SplashRequest(url=url, callback=self.parse, endpoint='render.html', args={'wait': 5})
+        yield SplashRequest(url=url, callback=self.parse_other_links, endpoint='execute',
+                            args={'wait': 5, 'lua_source': self.script}, dont_filter=True)
 
     def parse(self, response):
-        pass
+
+        for button in response.xpath("//li[contains(@class, 'pull-right')]"):
+            yield {
+                'pc': button.xpath("//li[contains(@class, 'pull-right')][1]/a/@href").extract(),
+                'xbox': button.xpath("//li[contains(@class, 'pull-right')][2]/a/@href").extract(),
+                'ps4': button.xpath("//li[contains(@class, 'pull-right')][3]/a/@href").extract()
+            }
+
+    def parse_other_links(self, response):
+        for link in response.data:
+            sel = Selector(text=link)
+            yield {
+                'pc': sel.xpath("//li[contains(@class, 'pull-right')][1]/a/@href").extract(),
+                'xbox': sel.xpath("//li[contains(@class, 'pull-right')][2]/a/@href").extract(),
+                'ps4': sel.xpath("//li[contains(@class, 'pull-right')][3]/a/@href").extract()
+            }
