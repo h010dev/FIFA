64b7700b5720ddddf016cd3f667ef1245831c384
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-03-28 14:19:47 +0700

    Added regex for deny attribute in LinkExtractor: prevents from scraping redundant sites. Added pagination feature as well.

--- fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/sofifa_detailed.py
+++ fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/sofifa_detailed.py
@@ -10,7 +10,9 @@ class SofifaDetailedSpider(CrawlSpider):
     start_urls = ['http://sofifa.com/players/']
 
     rules = (
-        Rule(LinkExtractor(restrict_xpaths="//a[contains(@href, 'player/')]"), callback='parse_item', follow=True)
+        Rule(LinkExtractor(deny=([r'\?', r'[0-9]+/[0-9]+/', r'/changeLog', r'/live', r'/squads', r'/calculator/']),
+                           restrict_xpaths="//a[contains(@href, 'player/')]"), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//a[text()='Next']"), callback='parse_item', follow=True)
     )
 
     def parse_item(self, response):
