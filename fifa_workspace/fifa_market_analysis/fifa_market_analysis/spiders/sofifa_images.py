0fc927f05e2bac39c192c57f1f179c52ce239c00
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-03-31 09:19:49 +0700

    Created new spider to test image crawling on sofifa. Tested output to img_test.json.

--- fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/sofifa_images.py
+++ fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/sofifa_images.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 
-class SofifaImagesSpider(scrapy.Spider):
+class SofifaDetailedSpider(CrawlSpider):
     name = 'sofifa_images'
-    allowed_domains = ['sofifa.com/players']
+    allowed_domains = ['sofifa.com']
     start_urls = ['http://sofifa.com/players/']
 
-    def parse(self, response):
-        pass
+    rules = (
+        Rule(LinkExtractor(deny=([r'\?', r'[0-9]+/[0-9]+/', r'/changeLog', r'/live', r'/squads', r'/calculator/']),
+                           restrict_xpaths="//a[contains(@href, 'player/')]"), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//a[text()='Next']"), callback='parse_item', follow=True)
+    )
+
+    def parse_item(self, response):
+
+        for img in response.xpath("//div/div/article/div/img"):
+            yield{
+                'img': img.xpath("./@data-src").get()
+            }
