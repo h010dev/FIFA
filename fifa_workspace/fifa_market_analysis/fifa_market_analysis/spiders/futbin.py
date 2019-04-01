84eea2a728be49aa4963dd836b88a121522608cf
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-04-01 12:33:49 +0700

    Testing spider on xbox market data: test successful.

--- fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/futbin.py
+++ fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/futbin.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json
 
 
 class FutbinSpider(scrapy.Spider):
+
     name = 'futbin'
     allowed_domains = ['www.futbin.com']
-    start_urls = ['http://www.futbin.com/']
+
+    def start_requests(self):
+        yield scrapy.Request(url="https://www.futbin.com/auctionsGraph?type=live_graph&console=XONE",
+                             callback=self.parse_market)
+
+    def parse_market(self, response):
+        data = json.loads(response.body)
+        with open('fut_market_test.json', 'w') as file:
+            file.write(json.dumps(data))
 
     def parse(self, response):
         pass
