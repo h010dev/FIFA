5c4cadca18d70e8c459de8002e0242b9e04a49bf
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-04-03 16:40:54 +0700

    Testing out new way to name images that are downloaded in pipeline: not working at the moment.

--- fifa_workspace/fifa_market_analysis/fifa_market_analysis/pipelines.py
+++ fifa_workspace/fifa_market_analysis/fifa_market_analysis/pipelines.py
@@ -5,6 +5,8 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from pymongo import MongoClient
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy import Request
 
 
 class MongoDBPipeline(object):
@@ -37,3 +39,36 @@ class MongoDBPipeline(object):
         # )
         self.db[self.collection].insert_one(dict(item))
         return item
+
+
+class ImagesToDownloadPipeline(ImagesPipeline):
+
+    def get_media_requests(self, item, info):
+        return [Request(x, meta={'id': item.get('id')}) for x in item.get(self.images_urls_field, [])]
+
+    def file_path(self, request, response=None, info=None):
+        ## start of deprecation warning block (can be removed in the future)
+        def _warn():
+            from scrapy.exceptions import ScrapyDeprecationWarning
+            import warnings
+            warnings.warn('ImagesPipeline.image_key(url) and file_key(url) methods are deprecated, '
+                          'please use file_path(request, response=None, info=None) instead',
+                          category=ScrapyDeprecationWarning, stacklevel=1)
+
+        # check if called from image_key or file_key with url as first argument
+        if not isinstance(request, Request):
+            _warn()
+            url = request
+        else:
+            url = request.url
+
+        # detect if file_key() or image_key() methods have been overridden
+        if not hasattr(self.file_key, '_base'):
+            _warn()
+            return self.file_key(url)
+        elif not hasattr(self.image_key, '_base'):
+            _warn()
+            return self.image_key(url)
+        ## end of deprecation warning block
+
+        return 'full/%s.jpg' % (request.meta['id'])
--- fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/sofifa_images.py
+++ fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/sofifa_images.py
@@ -6,45 +6,22 @@ from ..items import ImageItem
 from scrapy.loader import ItemLoader
 
 
-class SofifaDetailedSpider(CrawlSpider):
+class SofifaImagesSpider(CrawlSpider):
     name = 'sofifa_images'
     allowed_domains = ['sofifa.com']
     start_urls = ['http://sofifa.com/players/']
 
     rules = (
         Rule(LinkExtractor(deny=([r'\?', r'[0-9]+/[0-9]+/', r'/changeLog', r'/live', r'/squads', r'/calculator/']),
-                           restrict_xpaths="//a[contains(@href, 'player/')]"), callback='parse_item', follow=True),
-        Rule(LinkExtractor(restrict_xpaths="//a[text()='Next']"), callback='parse_item', follow=True)
+                           restrict_xpaths="//a[contains(@href, 'player/')]"), callback='parse', follow=True),
+        # Rule(LinkExtractor(restrict_xpaths="//a[text()='Next']"), callback='parse', follow=True)
     )
 
     def parse_item(self, response):
 
-        face = ImageItem()
-        flag = ImageItem()
-        club = ImageItem()
-        team = ImageItem()
-
-        face_url = []
-        flag_url = []
-        club_url = []
-        team_url = []
-
         for img in response.xpath("//div/div/article/div/img"):
-            face_url.append(img.xpath("./@data-src").get())
-            flag_url.append(response.xpath(".//div[@class='meta']/a/img/@data-src").get())
-            club_url.append(response.xpath(".//div/ul/li/figure/img/@data-src").getall()[0])
-            team_url.append(response.xpath(".//div/ul/li/figure/img/@data-src").getall()[-1])
-        face["image_urls"] = face_url
-        flag["image_urls"] = flag_url
-        club["image_urls"] = club_url
-        team["image_urls"] = team_url
-        return face, flag, club, team
-
-        # for img in response.xpath("//div/div/article/div/img"):
-        #     yield{
-        #         'name': response.xpath(".//div[@class='info']/h1/text()").get(),
-        #         'face': img.xpath("./@data-src").get(),
-        #         'flag': response.xpath(".//div[@class='meta']/a/img/@data-src").get(),
-        #         'club': response.xpath(".//div/ul/li/figure/img/@data-src").getall()[0],
-        #         'team': response.xpath(".//div/ul/li/figure/img/@data-src").getall()[-1]
-        #     }
+            loader = ItemLoader(item=ImageItem(), selector=img)
+            img_url = img.xpath("./@data-src").get()
+            loader.add_value('image_urls', img_url)
+            loader.add_xpath('id', ".//div[@class='info']/h1/text()")
+            yield loader.load_item()
