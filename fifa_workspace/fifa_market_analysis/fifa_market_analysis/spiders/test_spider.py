48cb17b3d2712b64790642a345f874ab0d304e9a
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-04-20 15:16:55 +0700

    Modified logging handler. Was giving permission errors when tested on player_pages spider. Working now.

--- fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/test_spider.py
+++ fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/test_spider.py
@@ -1,6 +1,5 @@
 import scrapy
 import logging
-import datetime
 from scrapy.utils.log import configure_logging
 from logging.handlers import TimedRotatingFileHandler
 
@@ -18,19 +17,20 @@ class TestSpider(scrapy.Spider):
 
     configure_logging(install_root_handler=False)
 
-    log_format = '%(levelname)s: %(message)s'
-    log_level = logging.DEBUG
-    log_file = 'test_log_file.log'
+    log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    log_level = logging.INFO
+    log_file = 'test_log.log'
+
     logging.basicConfig(
-        filename=log_file,
         format=log_format,
         level=log_level
     )
 
-    timed_file_log = TimedRotatingFileHandler(log_file, when='S', interval=1, backupCount=5)
-    timed_file_log.setFormatter(logging.Formatter(log_format))
+    rotating_file_log = TimedRotatingFileHandler(log_file, when='S', interval=1, backupCount=5)
+    rotating_file_log.setFormatter(logging.Formatter(log_format))
+
     root_logger = logging.getLogger()
-    root_logger.addHandler(timed_file_log)
+    root_logger.addHandler(rotating_file_log)
 
     def parse(self, response):
         for row in response.xpath("//div[@class='row']/div[@class='col-md-8']/div"):
