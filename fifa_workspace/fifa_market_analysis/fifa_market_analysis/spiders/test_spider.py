40f3d1b8cd94e8065c41fab2d78a80c81f7e122c
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-04-20 13:25:06 +0700

    Modified test_spider logger - added timed rotating handler, test successful.

--- fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/test_spider.py
+++ fifa_workspace/fifa_market_analysis/fifa_market_analysis/spiders/test_spider.py
@@ -1,4 +1,8 @@
 import scrapy
+import logging
+import datetime
+from scrapy.utils.log import configure_logging
+from logging.handlers import TimedRotatingFileHandler
 
 
 class TestSpider(scrapy.Spider):
@@ -12,10 +16,26 @@ class TestSpider(scrapy.Spider):
         }
     }
 
+    configure_logging(install_root_handler=False)
+
+    log_format = '%(levelname)s: %(message)s'
+    log_level = logging.DEBUG
+    log_file = 'test_log_file.log'
+    logging.basicConfig(
+        filename=log_file,
+        format=log_format,
+        level=log_level
+    )
+
+    timed_file_log = TimedRotatingFileHandler(log_file, when='S', interval=1, backupCount=5)
+    timed_file_log.setFormatter(logging.Formatter(log_format))
+    root_logger = logging.getLogger()
+    root_logger.addHandler(timed_file_log)
+
     def parse(self, response):
         for row in response.xpath("//div[@class='row']/div[@class='col-md-8']/div"):
             yield {
-                'quote': row.xpath(".//span[@class='text']/text()"),
+                'quote': row.xpath(".//span[@class='text']/text()").get(),
             }
 
         next_page = response.xpath("//li[@class='next']/a/@href").get()
