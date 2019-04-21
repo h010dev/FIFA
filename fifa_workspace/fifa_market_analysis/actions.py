1c4050b365d2efd8087c29ce28ceabaeda630eba
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-04-21 12:25:57 +0700

    Preparing project for move

--- fifa_workspace/fifa_market_analysis/actions.py
+++
@@ -1,9 +0,0 @@
-from spidermon.core.actions import Action
-
-
-class CloseSpiderAction(Action):
-
-    def run_action(self):
-        spider = self.data['spider']
-        spider.logger.info("Closing spider")
-        spider.crawler.engine.close_spider(spider, 'closed_by_spidermon')
