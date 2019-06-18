c2db2862bb6ef9b0a42081c485a93a264ac8d4df
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-06-18 12:06:45 -0400

    Updated sofifa_team_urls.py with new xpaths.

--- fifa_data/fifa_data/spiders/sofifa_team_urls.py
+++ fifa_data/fifa_data/spiders/sofifa_team_urls.py
@@ -1,85 +1,90 @@
 import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.loader import ItemLoader
-from fifa_data.items import NationalTeamStats
-from fifa_data.sofifa_settings import sofifa_settings
-from scrapy.utils.log import configure_logging
 from scrapy.crawler import CrawlerRunner
+from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.utils.log import configure_logging
+
 from twisted.internet import reactor
 
+from fifa_data.items import NationalTeamStats
+from fifa_data.sofifa_settings import sofifa_settings
 from proxies.proxy_generator import gen_proxy_list
 from user_agents.user_agent_generator import gen_useragent_list
 
+
 class SofifaTeamUrlsSpider(CrawlSpider):
+
+    """
+    Collects all team urls found on sofifa.com to be later scraped by
+    the SofifaClubPagesSpider. URLs are stored inside the team_urls
+    collection at mongodb://mongo_server:27017/sofifa
+    """
 
     name = 'team_pages'
 
     proxies = gen_proxy_list()
     user_agent = gen_useragent_list()
 
+    custom_settings = sofifa_settings(
+        name=name,
+        database='sofifa',
+        collection='team_urls',
+        proxies=proxies,
+        user_agent=user_agent,
+        validator='TeamItem'
+    )
+
     allowed_domains = [
         'sofifa.com'
     ]
 
     start_urls = [
-        'https://sofifa.com/teams/national/'
+        'https://sofifa.com/teams?type=national'
     ]
 
     rules = (
 
         Rule(
             LinkExtractor(
-                deny=(
-                    [
-                        r'\?',
-                        r'/[0-9]+',
-                        r'/forgot',
-                        r'/shortlist',
-                        r'/authorize',
-                        r'/leagues',
-                        r'/squad',
-                        r'/help',
-                        r'/compare',
-                        r'/players',
-                        r'/teams'
-                    ]
-                )
+                deny=([
+                    r'\?',
+                    r'/[0-9]+',
+                    r'/forgot',
+                    r'/shortlist',
+                    r'/authorize',
+                    r'/leagues',
+                    r'/squad',
+                    r'/help',
+                    r'/compare',
+                    r'/players',
+                    r'/player',
+                    r'/changeLog',
+                    r'/live',
+                    r'/calculator'
+                ]),
+                allow=([
+                    'https://sofifa.com/teams?type=national/'
+                ]),
             ),
             callback='parse_start_url',
             follow=True
         ),
-
         Rule(
-            LinkExtractor(
-                restrict_xpaths="//a[text()='Next']"
-            ),
+            LinkExtractor(restrict_xpaths="//a[text()='Next']"),
             callback='parse_item',
             follow=True
         )
     )
 
-    custom_settings = sofifa_settings(
-        name=name,
-        database='sofifa',
-        proxies=proxies,
-        user_agent=user_agent,
-        collection='team_urls',
-        validator='TeamItem'
-    )
-
     def parse_start_url(self, response):
-
-        """
-        Parse main page for data that is not available in extracted links.
-        """
 
         for row in response.xpath(
                 "//table[@class='table table-hover persist-area']/tbody/tr"
         ):
 
             loader = ItemLoader(
-                item=NationalTeamStats(),
+                item=TeamStatItem(),
                 selector=row,
                 response=response
             )
@@ -88,37 +93,29 @@ class SofifaTeamUrlsSpider(CrawlSpider):
                 'id',
                 ".//a[contains(@href, 'team/')]/@href"
             )
-
             loader.add_xpath(
                 'nationality',
-                ".//a[contains(@href, 'teams?na')]/text()"
+                ".//div/a[1]/@title"
             )
-
             loader.add_xpath(
                 'region',
-                ".//a[contains(@href, 'teams?ct')]/text()"
+                ".//td/a[1]/text()"
             )
-
             loader.add_xpath(
                 'num_players',
-                ".//td[@class='col text-center'][last()]/div/text()"
+                ".//td[@data-col='ps']/text()"
             )
-
             loader.add_xpath(
                 'hits',
-                ".//div[@class='col-comments text-right text-ellipsis rtl']\
-                /text()"
+                ".//td[@class='col-comment']/text()[1]"
             )
-
             loader.add_xpath(
                 'comments',
-                ".//div[@class='col-comments text-right text-ellipsis rtl']\
-                /text()"
+                ".//td[@class='col-comment']/text()[2]"
             )
-
             loader.add_xpath(
                 'team_page',
-                ".//a[contains(@href, 'team/')]/@href"
+                ".//td[2]/div/a[2]/@href"
             )
 
             yield loader.load_item()
