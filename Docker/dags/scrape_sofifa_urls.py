3d8c30809acf78d6b8f1622e3e69207b95b708d7
Mohamed Al-Hussein <sn95racing.v6@gmail.com>
2019-06-18 14:27:37 -0400

    Uncommented code to use in airflow testing.

--- Docker/dags/scrape_sofifa_urls.py
+++ Docker/dags/scrape_sofifa_urls.py
@@ -24,41 +24,41 @@ collect_club_urls = BashOperator(
     dag=dag
 )
 
-# collect_team_urls = BashOperator(
-#     task_id='get_teamurls',
-#     bash_command=\
-#     "cd /FIFA/fifa_data/ && python3 -m fifa_data.spiders.sofifa_team_urls ",
-#     dag=dag
-# )
-# 
-# collect_player_urls = BashOperator(
-#     task_id='get_playerurls',
-#     bash_command=\
-#     "cd /FIFA/fifa_data/ && python3 -m fifa_data.spiders.sofifa_player_urls ",
-#     dag=dag
-# )
-# 
-# scrape_club_urls = BashOperator(
-#     task_id='scrape_cluburls',
-#     bash_command=\
-#     "cd /FIFA/fifa_data/ && python3 -m fifa_data.spiders.sofifa_club_pages ",
-#     dag=dag
-# )
-# 
-# scrape_team_urls = BashOperator(
-#     task_id='scrape_teamurls',
-#     bash_command=\
-#     "cd /FIFA/fifa_data/ && python3 -m fifa_data.spiders.sofifa_team_pages ",
-#     dag=dag
-# )
-# 
-# scrape_player_urls = BashOperator(
-#     task_id='scrape_playerurls',
-#     bash_command=\
-#     "cd /FIFA/fifa_data/ && python3 -m fifa_data.spiders.sofifa_player_pages ",
-#     dag=dag
-# )
+collect_team_urls = BashOperator(
+    task_id='get_teamurls',
+    bash_command=\
+    "cd /FIFA/fifa_data/ && python3 -m fifa_data.spiders.sofifa_team_urls ",
+    dag=dag
+)
 
-# collect_club_urls >> scrape_club_urls
-# collect_team_urls >> scrape_team_urls
-# collect_player_urls >> scrape_player_urls
+collect_player_urls = BashOperator(
+    task_id='get_playerurls',
+    bash_command=\
+    "cd /FIFA/fifa_data/ && python3 -m fifa_data.spiders.sofifa_player_urls ",
+    dag=dag
+)
+
+scrape_club_urls = BashOperator(
+    task_id='scrape_cluburls',
+    bash_command=\
+    "cd /FIFA/fifa_data/ && python3 -m fifa_data.spiders.sofifa_club_pages ",
+    dag=dag
+)
+
+scrape_team_urls = BashOperator(
+    task_id='scrape_teamurls',
+    bash_command=\
+    "cd /FIFA/fifa_data/ && python3 -m fifa_data.spiders.sofifa_team_pages ",
+    dag=dag
+)
+
+scrape_player_urls = BashOperator(
+    task_id='scrape_playerurls',
+    bash_command=\
+    "cd /FIFA/fifa_data/ && python3 -m fifa_data.spiders.sofifa_player_pages ",
+    dag=dag
+)
+
+collect_club_urls >> scrape_club_urls
+collect_team_urls >> scrape_team_urls
+collect_player_urls >> scrape_player_urls
